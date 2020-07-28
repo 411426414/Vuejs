44d49c851264f65d7cfda5e4fdb95e13fa56a069
411426414 <411426414@qq.com>
2020-07-28 20:54:01 +0800

    filter函数的使用

--- 8.购物车案例/index.js
+++ 8.购物车案例/index.js
@@ -70,6 +70,8 @@ const app = new Vue({
       }
       return totalPrice
     }
+
+    // reduce
   },
   filters: {
     showPrice(price) {
@@ -77,3 +79,41 @@ const app = new Vue({
     }
   }
 })
+
+// 编程范式: 命令式编程/声明式编程
+// 编程范式: 面向对象编程(第一公民：对象)/函数式编程(第一公民：函数)
+// filter/map/reduce
+// filter中的回调函数有一个要求：必须返回一个Boolean值
+// true:当返回true时，函数内部会自动将这次回调的n加入到新的数組中
+// false: 当返回false,函数内部会过滤掉这次的n
+const nums = [10,90,20,80,30,60,70,50,40]
+
+// 1.filter函数的使用
+let newNums = nums.filter(function (n) {//n依次取出nums数组的元素
+  return n < 60   //从数组取得的元素与60相比，返回布尔值，如果为真加入到新的数組中，否则过滤掉不添加
+})
+console.log(newNums);
+
+
+// // 1.需求:取出所有小于60的数字
+// let newNums = []
+// for (let n in nums) {
+//   if (n<60){
+//     newNums.push(n)
+//   }
+// }
+// console.log(newNums);
+//
+// // 2.将所有小于100的数字进行转化:全部*2
+// newNum = []
+// for (let n of newNum) {
+//   newNum.push(n*2)
+// }
+// console.log(newNum);
+//
+// // 3.将所有new2Nums数字相加，得到最终的记过
+// let total = 0
+// for (let n in newNum) {
+//   total += n
+// }
+// console.log(total);
